7ece3f940bd79d8bf535d7c4f7b5130b77068f8e
Sneha <spisneha25@users.noreply.github.com>
2015-10-03 18:38:19 -0400

    Create rotatedsearch.java

---
+++ Arrays/rotatedsearch.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public int search(int[] nums, int target) 
+    {
+        int lo = 0;
+        int hi = nums.length - 1;
+        
+        while(lo <= hi)
+        {
+            int mi = lo + (hi - lo) / 2;
+            
+            if(nums[mi] == target)
+                return mi;
+            else if(nums[mi] <= nums[hi])
+            {
+                if(target > nums[mi] && target <= nums[hi])
+                    lo = mi + 1;
+                else
+                    hi = mi - 1;
+            }
+            else
+            {
+                if(target < nums[mi] && target >= nums[lo])
+                    hi = mi - 1;
+                else
+                    lo = mi + 1;
+            }
+            
+        }
+        
+        return -1;
+    }
+}
