ae7d0a9e48c6c9a59f848d0b09d9cb6fb09e47d3
Sneha <spisneha25@users.noreply.github.com>
2015-10-13 01:12:09 -0400

    Update excelcolumn.java

--- SmartHacks/excelcolumn.java
+++ SmartHacks/excelcolumn.java
@@ -6,8 +6,31 @@ public class Solution {
         for(int i = 0; i < l; i++)
         {
             ans += Math.pow(26, l-i-1)*(s.charAt(i) - 'A' + 1);
-            //System.out.println(ans);
         }
         return ans;
+    }
+    
+    public String convertToTitle(int n) 
+    {
+        String bf = new String();
+        
+        while(n > 0)
+        {
+            int d = n % 26;
+            
+            if(d == 0)
+            {
+                bf = "Z" + bf;
+                n = n / 26 - 1; 
+            }
+                
+            else
+            {
+                bf = (char)('A' + --d) + bf;
+                n = n / 26;
+            }
+        }
+        
+        return bf;
     }
 }
